82164561bde3e24e5b66ca530809c301e195f592
AndorkaDominik <dominik.andorka@gmail.com>
2024-07-06 21:42:12 +0200

    might solve

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Beautiful-Animation/"
+  base: "/Beautiful-Animation/",
+  build: {
+    rollupOptions: {
+      external: ['/vite.svg'],
+      onwarn: (warning, warn) => {
+        if (warning.code === 'UNRESOLVED_IMPORT') {
+          if (warning.source === '/vite.svg') {
+            return;
+          }
+        }
+        warn(warning);
+      }
+    }
+  }
 })
